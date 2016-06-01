6017df5e75d0771d74d892f2112638aaca571c70
Tech_Query <shiy007@qq.com>
2016-06-01 19:37:06 +0800

    【重写】从零开始开发 EasyImport.js 的 AMD 版本，暂时删去所有 v1.2 的代码；
    【新增】符合 AMD 规范的源代码、构建脚本；
    【同步】更新 EasyImport.js v1.2 文档、iQuery v1 基础库

---
+++ build/source.js
@@ -0,0 +1,14 @@
+({
+    baseUrl:         '../source',
+    name:            'EasyImport',
+    out:             '../EasyImport_OnePage.js',
+    optimize:        'none',
+    onBuildWrite:    function (iName) {
+        if (iName != 'EasyImport')  return arguments[2];
+
+        return arguments[2]
+            .replace(/^define[\s\S]+?(function \()[^\)]*/m,  "\n($1BOM, DOM, $")
+            .replace(/\s+var BOM.+?;/, '')
+            .replace(/\}\).$/,  "})(self, self.document, self.iQuery);");
+    }
+});
