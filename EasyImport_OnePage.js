dfc603d73b064837bc42d0ca5ce8e89d2c48bf8c
Tech_Query <shiy007@qq.com>
2016-06-03 17:05:41 +0800

    【新增】内置模块 require、exports、iQuery 导出    close  #2

--- EasyImport_OnePage.js
+++ EasyImport_OnePage.js
@@ -4342,6 +4342,18 @@ define('iQuery',  function () {
 
     var AMD_Module = { },  Root_Path = This_Script().replace(/[^\/]+$/, '');
 
+    function AbsolutePath(iPath) {
+        iPath = (iPath.match(/^(\w+:)?\/\//) ? '' : Root_Path)  +  iPath;
+
+        return  iPath.replace(/(\/[^\/]+\/)(\.{1,2})\//,  function () {
+            return  (arguments[2].length == 1)  ?  arguments[1]  :  '/';
+        });
+    }
+
+    function InnerRequire() {
+        return  AMD_Module[AbsolutePath( arguments[0] )].exports;
+    }
+
     function New_Module(iPath, iName, iDepend, iModule) {
         AMD_Module[iPath] = {
             name:        iName  ||  $.fileName(iPath),
@@ -4354,32 +4366,37 @@ define('iQuery',  function () {
         else if ($.isPlainObject( iModule ))
             $.extend(AMD_Module[iPath], {
                 executed:    true,
-                export:      iModule
+                exports:     iModule
             });
     }
 
     function AMD_Load(iPath) {
         var iRequire = [ ];
 
-        for (var i = 0, _Module_;  this.parents[i];  i++) {
-            _Module_ = AMD_Module[ this.parents[i] ];
+        for (var i = 0, _Module_;  this.parents[i];  i++)
+            switch ( this.parents[i] ) {
+                case 'require':    iRequire[i] = InnerRequire;    break;
+                case 'exports':    iRequire[i] = this.exports;    break;
+                case 'iQuery':     iRequire[i] = $;               break;
+                default:           {
+                    _Module_ = AMD_Module[ this.parents[i] ];
 
-            if (! _Module_) {
-                New_Module( this.parents[i] );
+                    if (! _Module_) {
+                        New_Module( this.parents[i] );
 
-                $('<script />').attr('src',  this.parents[i] + '.js')
-                    .appendTo('head');
-            } else if (! _Module_.executed) {
-                if (! (iPath in _Module_.children))
-                    _Module_.children[iPath] = true;
-            } else
-                iRequire[i] = _Module_.export;
-        }
-
+                        $('<script />').attr('src',  this.parents[i] + '.js')
+                            .appendTo('head');
+                    } else if (! _Module_.executed) {
+                        if (! (iPath in _Module_.children))
+                            _Module_.children[iPath] = true;
+                    } else
+                        iRequire[i] = _Module_.exports;
+                }
+            }
         if (iRequire.length < this.parents.length)  return;
 
         if ((! this.executed)  &&  this.defination) {
-            this.export = this.defination.apply(BOM, iRequire);
+            this.exports = this.defination.apply(BOM, iRequire);
             this.executed = true;
 
             console.log('AMD load:  ' + iPath + '.js');
@@ -4387,26 +4404,18 @@ define('iQuery',  function () {
     }
 
     BOM.define = function () {
-        var iArgs = $.makeArray(arguments);
+        var iArgs = $.makeArray(arguments),
+            iPath = This_Script().replace(/\.js(\?.*)?/, '');
 
-        var iName = (typeof iArgs[0] == 'string')  &&  iArgs.shift();
-        var iDepend = (iArgs[0] instanceof Array)  &&  iArgs.shift();
-        var iModule = iArgs[0];
-
-        var iPath = This_Script().replace(/\.js(\?.*)?/, '');
-
-        New_Module(iPath, iName, iDepend, iModule);
-
-        AMD_Module[iPath].parents = $.map(
-            AMD_Module[iPath].parents,
-            function (_Path_) {
-                _Path_ = (_Path_.match(/^(\w+:)?\/\//) ? '' : Root_Path)  +  _Path_;
-
-                return  _Path_.replace(/(\/[^\/]+\/)(\.{1,2})\//,  function () {
-                    return  (arguments[2].length == 1)  ?  arguments[1]  :  '/';
-                });
-            }
+        New_Module(
+            iPath,
+            (typeof iArgs[0] == 'string')  &&  iArgs.shift(),
+            (iArgs[0] instanceof Array)  &&  iArgs.shift(),
+            iArgs[0]
         );
+
+        AMD_Module[iPath].parents = $.map(AMD_Module[iPath].parents, AbsolutePath);
+
         $.each($.extend({ }, AMD_Module),  AMD_Load);
     };
 
